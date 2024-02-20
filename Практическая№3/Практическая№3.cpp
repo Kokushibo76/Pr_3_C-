5b297b07faf079ef2edf5c0bb1e4b01deb0e08dd
Kokushibo76 <nfrolov@gmail.com>
2024-02-20 20:19:19 +0300

    20:19_20.02.2024

---
+++ Практическая№3/Практическая№3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    setlocale(LC_ALL, "Russian");
+	const int line = 3;
+	const int cols = 5;
+	float arr[line][cols];
+    cout << "Введите элементы массива:\n";
+    for (int i = 0; i < line; ++i) {
+        cout << "Строка №" << i + 1 << ":\n";
+        for (int j = 0; j < cols; ++j) {
+            cin >> arr[i][j];
+        }
+    }
+    float ari[line];
+    for (int i = 0; i < line; ++i) {
+        float s = 0;
+        for (int j = 0; j < cols; ++j) {
+            s += arr[i][j];
+        }
+        ari[i] = s / cols;
+    }
+    cout << "\nСредние арифметические всех элементов:\n";
+    for (int i = 0; i < line; ++i) {
+        cout << "Строка №" << i + 1 << ": " << ari[i] << "\n";
+    }
+    return 0;
+}
